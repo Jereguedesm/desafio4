74495ad21e99818136e39f84719d830619cd4a06
Jeremias <jeremiasguedes04@gmail.com>
2023-09-01 21:32:31 -0300

    V_0.0.2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import multer from 'multer'
 import prodsRouter from "./routes/products.routes.js";
 import cartsRouter from './routes/carts.router.js';
 import { __dirname } from './path.js';
+import {engine} from "express-handlebars"
+import {Server} from "socket.io"
 import path from 'path';
 
 const PORT = 8080
@@ -18,10 +20,31 @@ const storage = multer.diskStorage({
     }
 })
 
+
+const server = app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
+
+
 //Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", path.resolve(__dirname, "./views"))
+
 const upload = multer({ storage: storage })
+
+//server de socket.io
+const io = new Server(server)
+
+io.on("connection", () => {
+    console.log("Servidor Socket.io conectado")
+})
+
+
+
 
 //Routes
 app.use('/api/products', prodsRouter)
@@ -34,6 +57,3 @@ app.post('/upload', upload.single('product'), (req, res) => {
     res.status(200).send("Imagen cargada")
 })
 
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
